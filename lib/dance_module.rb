b64142ce57298da3e2cbcb9df5a43e36da88f34e
J K <kj688895@gmail.com>
2020-09-27 21:44:41 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,3 +1,16 @@
 module Dance
+  def twirl
+    "I'm twirling!"
+  end
 
-end 
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  
+
+end
